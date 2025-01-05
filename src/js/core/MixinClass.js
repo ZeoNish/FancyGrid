cbb1d74161363a773d368772249d8d9a466ec027
ZeoNish <zeonish@gmail.com>
2025-01-05 19:08:54 +0300

    Update MixinClass.js

--- src/js/core/MixinClass.js
+++ src/js/core/MixinClass.js
@@ -2,73 +2,74 @@
  * @class Fancy.MixinClass
  * @constructor
  */
-Fancy.MixinClass = function(){};
+Fancy.MixinClass = function () {
+};
 
 Fancy.MixinClass.prototype = {
-  /**
-   * generate unique id for class
-   */
-  initId(){
-    const me = this,
-      prefix = me.prefix || Fancy.prefix;
+    /**
+     * generate unique id for class
+     */
+    initId() {
+        const me = this,
+            prefix = me.prefix || Fancy.prefix;
 
-    me.id = me.id || Fancy.id(null, prefix);
+        me.id = me.id || Fancy.id(null, prefix);
 
-    Fancy.addWidget(me.id, me);
-  },
-  /*
-   * Initialize plugins if they are presented in class
-   */
-  initPlugins() {
-    const me = this,
-      widget = me;
+        Fancy.addWidget(me.id, me);
+    },
+    /*
+     * Initialize plugins if they are presented in class
+     */
+    initPlugins() {
+        const me = this,
+            widget = me;
 
-    let plugin,
-      objectPlugin,
-      pluginConfig;
+        let plugin,
+            objectPlugin,
+            pluginConfig;
 
-    me.plugins = me.plugins || [];
+        me.plugins = me.plugins || [];
 
-    if (me._plugins) {
-      me.plugins = me.plugins.concat(me._plugins);
+        if (me._plugins) {
+            me.plugins = me.plugins.concat(me._plugins);
+        }
+
+        if (me.plugins !== undefined) {
+            me.$plugins = me.plugins;
+            delete me.plugins;
+        }
+
+        if (me.$plugins === undefined) {
+            return;
+        }
+
+        var i = 0,
+            plugins = me.$plugins,
+            iL = plugins.length,
+            inWidgetName,
+            types = {};
+
+        for (; i < iL; i++) {
+            pluginConfig = plugins[i];
+            pluginConfig.widget = widget;
+
+            const type = pluginConfig.type || pluginConfig.ptype;
+
+            if (types[type] === true) {
+                continue;
+            }
+
+            types[type] = true;
+            plugin = Fancy.getPluginByType(type);
+            if (plugin === undefined) {
+                throw new Error(`[FancyGrid Error] - some of module was not loaded. Grid plugin does not exist - ${type}`);
+            }
+            objectPlugin = new plugin(pluginConfig);
+            inWidgetName = pluginConfig.inWidgetName || objectPlugin.inWidgetName;
+
+            if (inWidgetName !== undefined) {
+                widget[inWidgetName] = objectPlugin;
+            }
+        }
     }
-
-    if(me.plugins !== undefined){
-      me.$plugins = me.plugins;
-      delete me.plugins;
-    }
-
-    if(me.$plugins === undefined){
-      return;
-    }
-
-    var i = 0,
-      plugins = me.$plugins,
-      iL = plugins.length,
-      inWidgetName,
-      types = {};
-
-    for(;i<iL;i++){
-      pluginConfig = plugins[i];
-      pluginConfig.widget = widget;
-
-      const type = pluginConfig.type || pluginConfig.ptype;
-
-      if (types[type] === true) {
-        continue;
-      }
-
-      types[type] = true;
-      plugin = Fancy.getPluginByType( type );
-      if(plugin === undefined){
-        throw new Error(`[FancyGrid Error] - some of module was not loaded. Grid plugin does not exist - ${type}`);
-      }
-      objectPlugin = new plugin(pluginConfig);
-      inWidgetName = pluginConfig.inWidgetName || objectPlugin.inWidgetName;
-
-      if(inWidgetName !== undefined){
-        widget[ inWidgetName ] = objectPlugin;
-      }
-    }
-  }
 };
